939cab205180503a903d6a3be45218f4e45963a8
ArthurCastro99 <arthurcastro092012@gmail.com>
2026-03-23 17:15:07 -0300

    test1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import MyButton from "./components/myButton";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Bem vindo
+      </h1>
+      <MyButton />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/myButton.jsx
@@ -0,0 +1,9 @@
+export default function MyButton() {
+  return (
+    <>
+        <button>
+            Eu sou um botão
+        </button>
+    </>
+  );
+}
